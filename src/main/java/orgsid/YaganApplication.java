31c1d523c7b3941ff9830e5ef3c1fd209146af3a
unknown <oussa@titi.corp.priv>
2019-04-24 23:10:59 +0200

    first commit

---
+++ src/main/java/orgsid/YaganApplication.java
@@ -0,0 +1,13 @@
+package orgsid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaganApplication.class, args);
+	}
+
+}
